d93cc341afed268463168ae18b528f1a886ec1d3
maciek.nierychlewski <mnierychlewski@sfscapital.com>
2020-11-13 13:11:03 +0100

    game engine

--- app/Models/Starship.php
+++ app/Models/Starship.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
 class Starship extends Eloquent
 {
     protected $connection = 'mongodb';
-    protected $collection = 'Starship';
+    protected $collection = 'Starships';
 
     protected $fillable = [
         'name', 'troops','group'
